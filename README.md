67963591f02e7675724eb67e0d9e0a87fbe764de
Grzegorz Walen <grzegorz.walen@swissborg.com>
2024-02-18 18:46:17 +0100

    add minting tokens to my_token example

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ### ErcStylus 
 
-Docs for OpenZeppelin home assignment
+## Deploy
+
+Deploy to Arbitrum Stylus testnet 
+```
+cargo stylus deploy \
+  --private-key-path=./.secret/pk.key
+```  
+
+Can verify the deployment on the block explorer : https://stylus-testnet-explorer.arbitrum.io/
+
+## Run scripts
+
+Run rust script from examples directory:
+
+`cargo run --example my_token --target=aarch64-apple-darwin`
--- examples/my_token.rs
+++ examples/my_token.rs
@@ -1,15 +1,15 @@
+use dotenv::dotenv;
 use ethers::{
     middleware::SignerMiddleware,
     prelude::abigen,
     providers::{Http, Middleware, Provider},
     signers::{LocalWallet, Signer},
-    types::Address,
+    types::{Address, TransactionReceipt, U256},
 };
 use eyre::eyre;
 use std::io::{BufRead, BufReader};
 use std::str::FromStr;
 use std::sync::Arc;
-use dotenv::dotenv;
 
 /// Your private key file path.
 const PRIV_KEY_PATH: &str = "PRIV_KEY_PATH";
@@ -20,32 +20,31 @@ const RPC_URL: &str = "RPC_URL";
 /// Deployed program address.
 const STYLUS_PROGRAM_ADDRESS: &str = "STYLUS_PROGRAM_ADDRESS";
 
+abigen!(
+    MyToken,
+    r#"[
+        function name() external view returns (string)
+        function symbol() external view returns (string)
+        function decimals() external view returns (uint8)
+        function totalSupply() external view returns (uint256)
+        function balanceOf(address account) external view returns (uint256)
+        function transfer(address recipient, uint256 amount) external returns (bool)
+        function allowance(address owner, address spender) external view returns (uint256)
+        function approve(address spender, uint256 amount) external returns (bool)
+        function transferFrom(address sender, address recipient, uint256 amount) external returns (bool)
+        function mint(address to, uint256 amount) external
+    ]"#
+);
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     dotenv().ok();
 
     let program_address = std::env::var(STYLUS_PROGRAM_ADDRESS)
         .map_err(|_| eyre!("No {} env var set", STYLUS_PROGRAM_ADDRESS))?;
-    let priv_key_path = std::env::var(PRIV_KEY_PATH)
-        .map_err(|_| eyre!("No {} env var set", PRIV_KEY_PATH))?;
-    let rpc_url =
-        std::env::var(RPC_URL).map_err(|_| eyre!("No {} env var set", RPC_URL))?;
-
-    abigen!(
-        MyToken,
-        r#"[
-            function name() external view returns (string)
-            function symbol() external view returns (string)
-            function decimals() external view returns (uint8)
-            function totalSupply() external view returns (uint256)
-            function balanceOf(address account) external view returns (uint256)
-            function transfer(address recipient, uint256 amount) external returns (bool)
-            function allowance(address owner, address spender) external view returns (uint256)
-            function approve(address spender, uint256 amount) external returns (bool)
-            function transferFrom(address sender, address recipient, uint256 amount) external returns (bool)
-            function mint(address to, uint256 amount) external
-        ]"#
-    );
+    let priv_key_path =
+        std::env::var(PRIV_KEY_PATH).map_err(|_| eyre!("No {} env var set", PRIV_KEY_PATH))?;
+    let rpc_url = std::env::var(RPC_URL).map_err(|_| eyre!("No {} env var set", RPC_URL))?;
 
     let provider = Provider::<Http>::try_from(rpc_url)?;
     let address: Address = program_address.parse()?;
@@ -59,13 +58,36 @@ async fn main() -> eyre::Result<()> {
     ));
 
     let my_token = MyToken::new(address, client);
+
+    // call MyToken contracts methods
+
     let token_name: String = my_token.name().call().await?;
     println!("token name: {}", token_name);
 
-    
+    mint_tokens(my_token, wallet).await?;
 
+    Ok(())
+}
 
-    Ok(())    
+async fn mint_tokens(
+    my_token: MyToken<SignerMiddleware<Provider<Http>, LocalWallet>>,
+    wallet: LocalWallet,
+) -> eyre::Result<()> {
+    let deployer_balance: U256 = my_token.balance_of(wallet.address()).call().await?;
+    println!("deployer balance before : {}", deployer_balance);
+
+    let mint_tx: TransactionReceipt = my_token
+        .mint(wallet.address(), 1000.into())
+        .send()
+        .await?
+        .await?
+        .expect("Mint tx returned non");
+    println!("mint tx: {:?}", mint_tx.transaction_hash);
+
+    let deployer_balance: U256 = my_token.balance_of(wallet.address()).call().await?;
+    println!("deployer balance after mint : {}", deployer_balance);
+
+    Ok(())
 }
 
 fn read_secret_from_file(fpath: &str) -> eyre::Result<String> {
