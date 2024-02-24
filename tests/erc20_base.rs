ccfb06261254d92e216d20e4287707450b12c46a
Grzegorz Walen <grzegorz.walen@swissborg.com>
2024-02-24 16:29:30 +0100

    tests for erc20 burnable

--- tests/erc20_base.rs
+++ tests/erc20_base.rs
@@ -8,6 +8,7 @@ use ethers::{
 };
 use eyre::{eyre, Report};
 use oz_stylus_erc::tokens::erc20::Erc20Params;
+use util::fixture_init::SharedFixtures;
 use std::str::FromStr;
 use std::sync::Arc;
 use tokio::sync::Mutex;
@@ -16,17 +17,7 @@ use tokio::sync::OnceCell;
 extern crate oz_stylus_erc;
 use crate::oz_stylus_erc::tokens::my_token::MyTokenParams;
 
-/// deployer private key file path.
-const ALICE_PRIV_KEY_PATH: &str = "ALICE_PRIV_KEY_PATH";
-
-/// deployer private key file path.
-const BOB_PRIV_KEY_PATH: &str = "BOB_PRIV_KEY_PATH";
-
-/// Stylus RPC endpoint url.
-const RPC_URL: &str = "RPC_URL";
-
-/// Deployed program address.
-const MY_TOKEN_PROGRAM_ADDRESS: &str = "STYLUS_PROGRAM_ADDRESS";
+mod util;
 
 abigen!(
     MyToken,
@@ -102,6 +93,7 @@ async fn mint_test() {
     assert_eq!(alice_balance_after - alice_balance_before, amount);
 }
 
+// TODO: remove - tested in burnable test file
 #[tokio::test]
 async fn burn_test() {
     let fixtures_mutex = init_fixtures().await.unwrap();
@@ -116,14 +108,12 @@ async fn burn_test() {
         .await
         .unwrap();
     let alice_balance_before = balance_of(token_signer_alice, alice_address).await.unwrap();
-    println!("alice_balance_before: {}", alice_balance_before);
 
     // burn and check the difference
     burn(token_signer_alice, amount)
         .await
         .unwrap();
     let alice_balance_after = balance_of(token_signer_alice, alice_address).await.unwrap();
-    println!("alice_balance_after: {}", alice_balance_after);
 
     assert_eq!(alice_balance_before - alice_balance_after, amount);
 }
@@ -250,8 +240,7 @@ async fn transfer_balance_too_small_error_test() {
     let alice_address = fixtures.alice_wallet.address();
     let bob_address = fixtures.bob_wallet.address();
     let token_signer_alice = &fixtures.token_signer_alice;
-    let amount_mint: U256 = 1000.into();
-    let amount_transfer: U256 = amount_mint * 2;
+    let amount: U256 = 1000.into();
 
     let alice_balance = token_signer_alice
         .balance_of(alice_address)
@@ -263,7 +252,7 @@ async fn transfer_balance_too_small_error_test() {
         .await
         .unwrap();
     // from alice to bob
-    let tx = transfer(token_signer_alice, bob_address, amount_transfer).await;
+    let tx = transfer(token_signer_alice, bob_address, amount).await;
 
     match tx {
         Ok(_) => panic!("transfer tx should fail"),
@@ -414,52 +403,21 @@ async fn transfer_from(
 async fn init_fixtures() -> eyre::Result<&'static Mutex<Fixtures>> {
     FIXTURES
         .get_or_try_init(|| async {
-            let fixtures = fill_fixtures().await?;
+            let fixtures = fill_local_fixtures().await?;
             Ok(Mutex::new(fixtures))
         })
         .await
 }
 
-async fn fill_fixtures() -> eyre::Result<Fixtures> {
-    dotenv().ok();
-
-    let program_address = std::env::var(MY_TOKEN_PROGRAM_ADDRESS)
-        .map_err(|_| eyre!("No {} env var set", MY_TOKEN_PROGRAM_ADDRESS))?;
-    let alice_key_path = std::env::var(ALICE_PRIV_KEY_PATH)
-        .map_err(|_| eyre!("No {} env var set", ALICE_PRIV_KEY_PATH))?;
-    let rpc_url = std::env::var(RPC_URL).map_err(|_| eyre!("No {} env var set", RPC_URL))?;
-    let bob_key_path = std::env::var(BOB_PRIV_KEY_PATH)
-        .map_err(|_| eyre!("No {} env var set", BOB_PRIV_KEY_PATH))?;
-
-    let provider = Provider::<Http>::try_from(rpc_url)?;
-    let my_token_address: Address = program_address.parse()?;
-
-    let alice_private_key = read_secret_from_file(&alice_key_path)?;
-    let alice_wallet = LocalWallet::from_str(&alice_private_key)?;
-    let chain_id = provider.get_chainid().await?.as_u64();
-    let alice_client = Arc::new(SignerMiddleware::new(
-        provider.clone(),
-        alice_wallet.clone().with_chain_id(chain_id),
-    ));
-
-    let bob_private_key = read_secret_from_file(&bob_key_path)?;
-    let bob_wallet = LocalWallet::from_str(&bob_private_key)?;
-    let bob_client = Arc::new(SignerMiddleware::new(
-        provider.clone(),
-        bob_wallet.clone().with_chain_id(chain_id),
-    ));
-
-    let token_signer_alice = MyToken::new(my_token_address, alice_client.clone());
-    let token_signer_bob = MyToken::new(my_token_address, bob_client.clone());
+async fn fill_local_fixtures() -> eyre::Result<Fixtures> {
+    let shared_fixture: SharedFixtures = util::fixture_init::fill_fixtures().await?;
+    let token_signer_alice = MyToken::new(shared_fixture.token_address, shared_fixture.alice_client.clone());
+    let token_signer_bob = MyToken::new(shared_fixture.token_address, shared_fixture.bob_client.clone());
 
     Ok(Fixtures {
-        alice_wallet,
-        bob_wallet,
+        alice_wallet: shared_fixture.alice_wallet,
+        bob_wallet: shared_fixture.bob_wallet,
         token_signer_alice,
         token_signer_bob,
     })
-}
-
-fn read_secret_from_file(fpath: &str) -> eyre::Result<String> {
-    Ok(std::fs::read_to_string(fpath)?)
 }
